258047efef9eed46b3b7123fa6fd57093c83fb6d
intoxicated <yakle1218@gmail.com>
2019-05-18 14:06:21 +0900

    initial commit

---
+++ Mobile/iOS/Extractor/Extractor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Extractor
+//
+//  Created by R3alFr3e on 5/18/19.
+//  Copyright © 2019 Esticker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
